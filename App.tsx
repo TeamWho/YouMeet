1b922d67c38fdb20156c234855a286cb35ec4233
Fembulvetr <113340525+Fembulvetr@users.noreply.github.com>
2022-12-09 18:10:13 +0200

    feat: tabbar navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Chat from './src/modules/chat';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SvgHome from './components/svg/svgHome';
+import SvgChat from './components/svg/chatSvg';
 import { Routes } from './src/types/navigation';
 import Home from './src/modules/home';
+import Chat from './src/modules/chat';
 
-
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={Routes.Home} component={Home} />
-                <Stack.Screen name={Routes.Chat} component={Chat} />
-            </Stack.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name={Routes.Home} component={Home} options={{ headerShown: false, tabBarIcon: () => <SvgHome /> }} />
+                <Tab.Screen name={Routes.Chat} component={Chat} options={{ headerShown: false, tabBarIcon: () => <SvgChat /> }} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
