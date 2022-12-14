5c79569829926be9257452d5c4c401cb639b613c
Artem Ivanov <artwood30@gmail.com>
2022-12-14 14:17:02 +0200

    fix: resolve merge conflicts consequences

--- App.tsx
+++ App.tsx
@@ -1,51 +1,62 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Routes } from './src/types/navigation';
-import Home from './src/modules/home';
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Chat from './src/modules/chat';
-import { View } from 'react-native';
+import Home from './src/modules/home';
+import { Routes } from './src/types/navigation';
 
 const Tab = createBottomTabNavigator();
 
 const tabNavigatorParams = {
-  tabBarShowLabel: false,
-  tabBarStyle: {
-    backgroundColor: '#272d2f',
-    borderTopWidth: 0,
+    tabBarShowLabel: false,
+    tabBarStyle: {
+        backgroundColor: '#272d2f',
+        borderTopWidth: 0,
+    }
 };
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator 
-        screenOptions={({ route }) => ({
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            backgroundColor: '#272d2f',
-            borderTopWidth: 0,
-}})}>
-        <Tab.Screen name="Home" component={Home} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
-          <View style={{
-            shadowColor: focused ? '#fff' : '#272d2f',
-            shadowOffset: {width: 0, height: 0},
-            shadowOpacity: 1.0,
-            shadowRadius: 10,
-          }}>
-            <FontAwesome name='home' size={24} color={focused ? '#fff' : '#585d60'}></FontAwesome>
-          </View>
-   ) }} />
-        <Tab.Screen name="Chat" component={Chat} options={{ headerShown: false, tabBarIcon: ({ focused }) => (
-          <View style={{
-            shadowColor: focused ? '#fff' : '#272d2f',
-            shadowOffset: {width: 0, height: 0},
-            shadowOpacity: 1.0,
-            shadowRadius: 10,
-          }}>
-            <FontAwesome name='commenting' size={24} color={focused ? '#fff' : '#585d60'}></FontAwesome>
-          </View>
-   )}} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={tabNavigatorParams}>
+                <Tab.Screen
+                    name={Routes.Home}
+                    component={Home}
+                    options={{ headerShown: false, tabBarIcon: ({ focused }) => (
+                        <View style={styles.iconWrapper}>
+                            <FontAwesome 
+                                name='home'
+                                size={24}
+                                color={focused ? '#fff' : '#585d60'}
+                            />
+                        </View>
+                    ) 
+                    }} 
+                />
+                <Tab.Screen 
+                    name={Routes.Chat}
+                    component={Chat}
+                    options={{ headerShown: false, tabBarIcon: ({ focused }) => (
+                        <View style={styles.iconWrapper}>
+                            <FontAwesome 
+                                name='commenting'
+                                size={24}
+                                color={focused ? '#fff' : '#585d60'}
+                            />
+                        </View>
+                    )}} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
+
+const styles = StyleSheet.create({
+    iconWrapper: {
+        // shadowColor: focused ? '#fff' : '#272d2f',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1.0,
+        shadowRadius: 10,
+    }
+});
